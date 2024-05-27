a968f217e9ed98a9bf6c4b49a2344d0fb2555756
Igor Laminsi <silvereigorlaminsi@gmail.com>
2024-05-27 17:45:58 +0200

    Initial commit

---
+++ Controllers/BotController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
+namespace EchoBot.Controllers
+{
+    [Route("api/messages")] // Route existante
+    [Route("botframework-directline/conversations")] // Nouvelle route ajoutée
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        private readonly IBotFrameworkHttpAdapter _adapter;
+        private readonly IBot _bot;
+
+        public BotController(IBotFrameworkHttpAdapter adapter, IBot bot)
+        {
+            _adapter = adapter;
+            _bot = bot;
+        }
+
+        [HttpPost]
+        [HttpGet]
+        public async Task PostAsync()
+        {
+            // Traite les requêtes HTTP POST et GET
+            await _adapter.ProcessAsync(Request, Response, _bot);
+        }
+    }
+}
